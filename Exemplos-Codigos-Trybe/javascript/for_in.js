7c542e12e6628ad1af9ec46112b6542edc8f0abd
Fernando <pupygreen@gmail.com>
2022-07-24 16:08:43 -0300

    Usando 'For in'.

---
+++ Exemplos-Codigos-Trybe/javascript/for_in.js
@@ -0,0 +1,8 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+for (let i in numbers) {
+  console.log(`Indice = ${i}, número = ${numbers[i]}`);
+}
+
+console.log(`Acessando apenas o valor = ${numbers[9]}, que está no índice 9`);
