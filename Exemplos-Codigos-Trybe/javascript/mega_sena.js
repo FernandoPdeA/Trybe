6b77924c457e907804955e57739c8e5b97042011
Fernando <pupygreen@gmail.com>
2022-07-25 21:14:17 -0300

    Aterando código da mega-sena

--- Exemplos-Codigos-Trybe/javascript/mega_sena.js
+++ Exemplos-Codigos-Trybe/javascript/mega_sena.js
@@ -1,65 +1,46 @@
-//Jogo sorteado
-// let numeri1 = Math.floor(Math.random() * 60) + 1;
-// let numeri2 = Math.floor(Math.random() * 60) + 1;
-// let numeri3 = Math.floor(Math.random() * 60) + 1;
-// let numeri4 = Math.floor(Math.random() * 60) + 1;
-// let numeri5 = Math.floor(Math.random() * 60) + 1;
-// let numeri6 = Math.floor(Math.random() * 60) + 1;
-// let megaSenaNumbers = [numeri1, numeri2, numeri3, numeri4, numeri5, numeri6];
-
-//jogadores
-let jogo = [1, 2, 3, 4, 5, 6];
-let jogoJoao = [2, 4, 6, 8, 9, 10];
-let jogoLucas = [1, 2, 4, 6, 8, 9];
-let jogoFernando = [3, 5, 7, 8, 9, 10];
+//Jogo do Fernando
+let jogoFernando = [8, 21, 27, 5, 15, 18];
 
 //Jogo sorteado
 let megaSenaNumbers = [];
 
-for (let i = 0; i < 6; i++) {
-  //Não pode ser repetido
-  let randomNumber = Math.floor(Math.random() * 10) + 1;
+for (let i = 0; i < 6; i++) {  //Sorteia apenas 6 números 
 
-  while (megaSenaNumbers.includes(randomNumber)) {
-    randomNumber = megaSenaNumbers[Math.floor(Math.random() * 10) + 1];
+  let randomNumber = Math.floor(Math.random() * 60) + 1; //Gera os números aleatórios...
+
+  while (megaSenaNumbers.includes(randomNumber)) { // Verificar se o número já existe no array
+    randomNumber = megaSenaNumbers[Math.floor(Math.random() * 60) + 1]; // Gera novos números até que não existam números repetidos
   }
-  megaSenaNumbers.push(randomNumber);
+  megaSenaNumbers.push(randomNumber); //Adicionar números ao array
 }
 
-//quantidade de numeros acertados, como o jogo sorteado
+//Contador de acertos
 let acertos = 0;
 
-function nacertos(jogo, megaSenaNumbers) {
+function numAcertos(jogoFernando, megaSenaNumbers) {  //Função para contar os acertos
 
-  for (let index = 0; index < megaSenaNumbers.length; index++) {
-    for (let index2 = 0; index2 < jogo.length; index2++) {
-      if (megaSenaNumbers[index] === jogo[index2]) {
-        console.log(`O número ${megaSenaNumbers[index]} está na posição ${index2 + 1}`);
-        acertos++;
+  for (let index = 0; index < megaSenaNumbers.length; index++) {  //Verifica se o número existe no jogo sorteado  
+    for (let index2 = 0; index2 < jogoFernando.length; index2++) {  //Verifica se o número existe no jogo do fernando
+      if (megaSenaNumbers[index] === jogoFernando[index2]) {  //Verifica se o número existe no jogo do fernando
+        console.log(`O número ${megaSenaNumbers[index]} está na posição ${index2 + 1}`);  //Imprime o número e a posição
+        acertos++; //Incrementa o contador de acertos
       }
-    } if (acertos === 0) {
-      console.log(`O número ${jogo[index]} não está na lista`);
     }
+  } if (acertos === 0) { //Se não houver acertos
+    console.log('Que pena, não acertou nenhum número :/'); //Imprime mensagem
+    console.log('----------------------------------------------------');
   }
-  return acertos;
+  return acertos; //Retorna o contador de acertos
 }
 
 
+console.log(`Jogo sorteado = ${megaSenaNumbers}`);  //Imprime o jogo sorteado
 
-console.log(`Jogo sorteado = ${megaSenaNumbers}`);
 console.log('----------------------------------------------------');
-console.log(`Jogo do João = ${jogoJoao}`);
-console.log('----------------------------------------------------');
-console.log(`Jogo do Lucas = ${jogoLucas}`);
-console.log('----------------------------------------------------');
-console.log(`Jogo do Fernando = ${jogoFernando}`);
-console.log('----------------------------------------------------');
-// console.log(`Jogo padrão = ${jogo}`);
-console.log('----------------------------------------------------');
-console.log(`Quantidade de acertos do João = ${nacertos(megaSenaNumbers, jogoJoao)}`);
-console.log('----------------------------------------------------');
-console.log(`Quantidade de acertos do Lucas = ${nacertos(megaSenaNumbers, jogoLucas)}`);
-console.log('----------------------------------------------------');
-console.log(`Quantidade de acertos do Fernando = ${nacertos(megaSenaNumbers, jogoFernando)}`);
 
-//Quantidade de acerttos
+console.log(`Jogo do Fernando = ${jogoFernando}`);  //Imprime o jogo do fernando
+
+console.log('----------------------------------------------------');
+
+console.log(`Quantidade de acertos do Fernando = ${numAcertos(megaSenaNumbers, jogoFernando)}`);  //Imprime a quantidade de acertos do fernando
+
